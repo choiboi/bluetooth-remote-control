0c7e03baf63a098b2f2eef68a31ac7e92dbeeac1
choiboi <james.choiboi@gmail.com>
2012-12-14 22:30:06 -0500

    First commit for server.

---
+++ BluetoothRemoteControlServer/src/com/choiboi/apps/remotebluetoothserver/BluetoothServer.java
@@ -0,0 +1,13 @@
+package com.choiboi.apps.remotebluetoothserver;
+
+public class BluetoothServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!!");
+	}
+
+}
