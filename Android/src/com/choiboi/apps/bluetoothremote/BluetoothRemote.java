ad98cc7e811912a9606e9e7ed6f774fd6778cff8
James Choi <james.choiboi@gmail.com>
2012-12-16 21:23:32 -0500

    Halfway to basic implementation.

--- Android/src/com/choiboi/apps/bluetoothremote/BluetoothRemote.java
+++ Android/src/com/choiboi/apps/bluetoothremote/BluetoothRemote.java
@@ -1,22 +1,99 @@
 package com.choiboi.apps.bluetoothremote;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
 
 public class BluetoothRemote extends Activity {
+	
+	// Message types sent from the BluetoothService Handler
+	public static final int MESSAGE_STATE_CHANGE = 1;
+	public static final int MESSAGE_TOAST = 5;
+	
+	// Key names received from the BluetoothService Handler
+	public static final String TOAST = "toast";
+	
+	// Intent request codes
+	private static final int REQUEST_ENABLE_BLUETOOTH = 1;
+	
+	// Local Bluetooth adapter
+	private BluetoothAdapter mBluetoothAdapter;
+	private BluetoothService mBluetoothService;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// Setup the window layout
 		setContentView(R.layout.main);
+		
+		// Get default Bluetooth adapter
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		
+		// mBluetoothAdapter will be null if Bluetooth is not supported
+		if (mBluetoothAdapter == null) {
+			Toast.makeText(this, "Bluetooth is NOT Available!", Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		}
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
+		// Request Bluetooth to be turned on, if it is not on
+		if (!mBluetoothAdapter.isEnabled()) {
+			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH);
+		// Otherwise, setup command service
+		} else {
+			// Initialize Bluetooth service to handle connections
+			if (mBluetoothService == null) {
+				mBluetoothService = new BluetoothService(this, mHandler);
+			}
+		}
+	}
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
 	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (mBluetoothService != null) {
+			if (mBluetoothService.getState() == BluetoothService.STATE_NONE) {
+				mBluetoothService.start();
+			}
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+	
+	// Handler gets the information back from the CommandService
+	private final Handler mHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			}
+		}
+	};
 }
--- Android/src/com/choiboi/apps/bluetoothremote/BluetoothService.java
+++ Android/src/com/choiboi/apps/bluetoothremote/BluetoothService.java
@@ -1,15 +1,28 @@
 package com.choiboi.apps.bluetoothremote;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 
 public class BluetoothService {
 	
 	// Member fields
-	private BluetoothAdapter mBluetoothAdapter;
-	private Handler mHandler;
+	private final BluetoothAdapter mBluetoothAdapter;
+	private final Handler mHandler;
+	private ConnectThread mConnectThread;
+	private ConnectedThread mConnectedThread;
 	private int mState;
+	
+	// Unique UUID
+	private static final UUID _UUID = UUID.fromString("C46C11A93E424F64AB1EFC892E87B9DE");
 	
 	// Constants that indicate the current connection state
 	public static final int STATE_NONE = 0;		// Doing nothing
@@ -17,14 +30,86 @@ public class BluetoothService {
 	public static final int STATE_CONNECTING = 2;	// Listen for outgoing connections
 	public static final int STATE_CONNECTED = 3;	// Currently connected to a device
 	
+	// For Debugging purposes
+	private static final String TAG = "BluetoothService";
+	
 	public BluetoothService(Context context, Handler handler) {
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		mState = STATE_NONE;
 		mHandler = handler;
 	}
 	
+	/*
+	 * Start Bluetooth service. Begin a session in listening (server) mode.
+	 */
 	public synchronized void start() {
+		if (mConnectThread != null) {
+			mConnectThread.cancel();
+			mConnectThread = null;
+		}
+		if (mConnectedThread != null) {
+			mConnectedThread.cancel();
+			mConnectedThread = null;
+		}
 		
+		setState(STATE_LISTEN);
+	}
+	
+	/*
+	 * Start the ConnectThread to initiate a connection to a remote
+	 * device.
+	 * @param device The Bluetooth device to connect
+	 */
+	public synchronized void connect(BluetoothDevice device) {
+		// Cancel any thread attempting to make a connection
+		if (mState == STATE_CONNECTING) {
+			if (mConnectThread != null) {
+				mConnectThread.cancel();
+				mConnectThread = null;
+			}
+		}
+		
+		// Cancel any thread currently running a connection
+		if (mConnectedThread != null) {
+			mConnectedThread.cancel();
+			mConnectedThread = null;
+		}
+		
+		// Start a thread to connect with a given device
+		mConnectThread = new ConnectThread(device);
+		mConnectThread.start();
+		setState(STATE_CONNECTING);
+	}
+	
+	public synchronized void connected(BluetoothSocket socket, BluetoothDevice device) {
+		
+	}
+	
+	
+	/*
+	 * Indicate that the connection attempt failed and notify the UI Activity.
+	 */
+	private void connectionFailed() {
+		// Send a failure message back to the Activity
+		Message msg = mHandler.obtainMessage(BluetoothRemote.MESSAGE_TOAST);
+		Bundle bundle = new Bundle();
+		bundle.putString(BluetoothRemote.TOAST, "Unable to connect device");
+		msg.setData(bundle);
+		mHandler.sendMessage(msg);
+		
+		// Start the service over the restart listening mode
+		BluetoothService.this.start();
+	}
+	
+	/*
+	 * Set the state of the current connection.
+	 * @param state An integer defining the current connection state
+	 */
+	private synchronized void setState(int state) {
+		mState = state;
+		
+		// Give the new state to the Handler to update the UI Activity
+		mHandler.obtainMessage(BluetoothRemote.MESSAGE_STATE_CHANGE, state, -1).sendToTarget();
 	}
 	
 	/*
@@ -32,5 +117,73 @@ public class BluetoothService {
 	 */
 	public synchronized int getState() {
 		return mState;
+	}
+	
+	private class ConnectThread extends Thread {
+		private final BluetoothSocket mmSocket;
+		private final BluetoothDevice mmDevice;
+
+		public ConnectThread(BluetoothDevice device) {
+			mmDevice = device;
+			BluetoothSocket tempSocket = null;
+			
+			try {
+				tempSocket = device.createRfcommSocketToServiceRecord(_UUID);
+			} catch(Exception e) {
+				Log.e(TAG, "create Failed ====== ConnectThread");
+			}
+			
+			mmSocket = tempSocket;
+		}
+		
+		public void run() {
+			setName("ConnectThread");
+			
+			// Cancel discovery because it will slow down the connection.
+			mBluetoothAdapter.cancelDiscovery();
+			
+			try {
+				mmSocket.connect();
+			} catch (IOException e) {
+				connectionFailed();
+				
+				// Close the socket.
+				try {
+					mmSocket.close();
+				} catch (IOException e2) {
+					Log.e(TAG, "Unable to close() socket during connection failure.",e2);
+				}
+				
+				BluetoothService.this.start();
+				return;
+			}
+			
+			// Reset the ConnectThread because we are done
+			synchronized (BluetoothService.this) {
+				mConnectThread = null;
+			}
+			
+			// Start the connected thread
+			connected(mmSocket, mmDevice);
+		}
+
+		public void cancel() {
+			try {
+				mmSocket.close();
+			} catch (IOException e) {
+				Log.e(TAG, "close() of connect failed.", e);
+			}
+		}
+	}
+	
+	private class ConnectedThread extends Thread {
+		public ConnectedThread(BluetoothSocket socket) {
+			
+		}
+
+		public void cancel() {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 }
